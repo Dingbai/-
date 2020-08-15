887b1653efac4700440683f248581dde2f73b397
Dingbai <757586407@qq.com>
2020-08-15 12:57:50 +0800

    test: master change

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <!-- <p>hello nihao</p> -->
 		<!-- <router-link to="/HelloWorld">按钮</router-link> -->
-		hello world test git merge
+		hello world test git merge jojo 我不做人了
     <router-view />
   </div>
 </template>
@@ -11,7 +11,7 @@
 export default {
   name: 'App',
   created () {
-    // console.log(this.$router)
+    console.log(this.$router)
   }
 }
 </script>
